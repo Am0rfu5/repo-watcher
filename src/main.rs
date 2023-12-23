a42ca87f10eb6a7c194524322c8f00a3ad53a3e4
Am0rfu5 <am0fu5@protonmail.me>
2023-12-22 21:54:32 -0600

    add most basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,102 @@
 use clap::Parser;
-use git2::{Repository, Error};
+use git2::{Repository, MergeOptions, Error};
+use anyhow::{Context, Result};
 
 /// Monitors a GitHub repository for changes and pulls them
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
-    /// GitHub repository URL to monitor
-    #[clap(short, long)]
-    repo_url: String,
 
     /// Local repository path
     #[clap(short, long)]
     local_path: String,
+    
+    /// GitHub repository URL to monitor
+    #[clap(short, long)]
+    remote: String,
+    
+    /// Branch to monitor
+    #[clap(short, long)]
+    branch: String,
+    
 }
 
-fn pull_repo(repo_path: &str) -> Result<(), Error> {
+fn pull_repo(repo_path: &str, remote_name: &str, branch: &str) -> Result<(), Error> {
     let repo = Repository::open(repo_path)?;
-    let mut remote = repo.find_remote("origin")?;
+    let mut remote = repo.find_remote(remote_name)?;
 
-    remote.fetch(&["master"], None, None)?;
-    // Additional logic to merge fetched changes
+    remote.fetch(&[branch], None, None)?;
 
+    let fetch_head = repo.find_reference("FETCH_HEAD").unwrap();
+    let merge_commit = repo.reference_to_annotated_commit(&fetch_head).unwrap();
+
+    let mut merge_options = MergeOptions::new();
+    merge_options.fail_on_conflict(true);
+    repo.merge(&[&merge_commit], Some(&mut merge_options), None).unwrap();
     Ok(())
 }
 
-fn main() {
-    let cli = Cli::parse();
+fn fetch_latest_commit_sha(repo_path: &str, remote_name: &str) -> Result<String, Error> {
+    let repo = Repository::open(repo_path)?;
+    let mut remote = repo.find_remote(remote_name)?;
 
-    let repo_url = &cli.repo_url;
-    let local_path = &cli.local_path;
+    remote.fetch(&["branch"], None, None)?;
 
-    println!("Monitoring {} for changes", repo_url);
+    let fetch_head = repo.find_reference("FETCH_HEAD").unwrap();
+    let fetch_commit = repo.reference_to_annotated_commit(&fetch_head).unwrap();
+    
+    Ok(fetch_commit.id().to_string())
 }
 
-// TODO Logging
-// TODO Monitor
-// TODO Pull or Fetch
-// TODO Merge
-// TODO .env file command line option
-// TODO Github API integration
-// TODO Github webhook integration
-// TODO Github webhook secret
-// TODO Github webhook event type
-// TODO Github webhook event action
-// TODO Github webhook event repository
-// TODO Github webhook event sender
-// TODO Github webhook event organization
-// TODO Github webhook event installation
+fn check_for_new_commits(repo_path: &str, latest_sha: &str) -> Result<bool, Error> {
+    let repo = Repository::open(repo_path)?;
+    let head = repo.head()?.peel_to_commit()?;
+    let local_sha = head.id().to_string();
+
+    Ok(local_sha != latest_sha)
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    let local_path = &cli.local_path;
+    let remote = &cli.remote;
+    let branch = &cli.branch;
+    
+    let latest_sha = fetch_latest_commit_sha(local_path, remote)
+        .context("Failed to fetch the latest commit SHA")?;
+    let has_new_commits = check_for_new_commits(&cli.local_path, &latest_sha)
+        .context("Failed to check for new commits")?;
+
+    if has_new_commits {
+        pull_repo(&cli.local_path, remote, branch).context("Failed to pull new commits")?;        
+    }
+    
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fetch_latest_commit_sha() {
+        let local_path = "test_repo";
+        let remote_name = "origin";
+        let expected_sha = "a4a7dce85cf63874e984719f4fdd239f5145052f";
+
+        let actual_sha = fetch_latest_commit_sha(local_path, remote_name).unwrap();
+
+        assert_eq!(expected_sha, actual_sha);
+    }
+
+    #[test]
+    fn test_check_for_new_commits() {
+        let local_path = "test_repo";
+        let latest_sha = "a4a7dce85cf63874e984719f4fdd239f5145052f";
+
+        let has_new_commits = check_for_new_commits(local_path, latest_sha).unwrap();
+
+        assert_eq!(false, has_new_commits);
+    }
+}
